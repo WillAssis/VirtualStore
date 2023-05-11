e077b357d1a83ec73ba3351a6df82a38a2e26f20
Artur Cirino <artur.cirino@gmail.com>
2023-05-11 17:06:26 -0300

    Add pages

---
+++ src/pages/Cart.tsx
@@ -0,0 +1,9 @@
+function Cart() {
+  return (
+    <div>
+      <h1>Cart</h1>
+    </div>
+  );
+}
+
+export default Cart;
---
+++ src/pages/ProductDetails.tsx
@@ -0,0 +1,9 @@
+function ProductDetails() {
+  return (
+    <div>
+      <h1>Product Details</h1>
+    </div>
+  );
+}
+
+export default ProductDetails;
