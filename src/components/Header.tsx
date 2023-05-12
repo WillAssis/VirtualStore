5741f63bcd6411c8308252e32d751af98adc4bad
Artur Cirino <artur.cirino@gmail.com>
2023-05-12 15:19:39 -0300

    Added Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/produtos">Produtos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
