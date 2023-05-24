0c7a2accd77f0f8abb36978326ccdb9b24869176
Artur Cirino <artur.cirino@gmail.com>
2023-05-24 15:45:23 -0300

    Adding ProductSearch, ProductCard and Pagination in products page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,22 +1,45 @@
-import { Link } from 'react-router-dom';
-import products from '../data';
+import { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import products from "../data";
+import ProductCard from "../components/ProductCard/ProductCard";
+import ProductSearch from "../components/ProductSearch/ProductSearch";
+import Pagination from "../components/Pagination/Pagination";
 
 function Products() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 8;
+
+  // Filtra os produtos com base na pesquisa
+  const [filteredProducts, setFilteredProducts] = useState(products);
+
+  // Lógica de paginação
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  // Função para mudar a página atual
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
-    <div>
-      <h1>Produtos</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h3>{product.title}</h3>
-          <Link to={`/produtos/${product.slug}`}>
-            <img src={product.image} alt={product.title} width="300" />
-          </Link>
-          <p>{product.description}</p>
-          <Link to={`/produtos/${product.slug}`}>Ver detalhes</Link>
-          <hr />
-        </div>
-      ))}
-    </div>
+    <Container>
+      <h4 className="mt-3">Mostrando Produtos:</h4>
+      <ProductSearch products={products} setFilteredProducts={setFilteredProducts} />
+      <Row>
+        {currentProducts.map((product) => (
+          <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <ProductCard product={product} />
+          </Col>
+        ))}
+      </Row>
+      <Pagination
+        currentPage={currentPage}
+        productsPerPage={productsPerPage}
+        totalProducts={filteredProducts.length}
+        paginate={paginate}
+      />
+    </Container>
   );
 }
 
