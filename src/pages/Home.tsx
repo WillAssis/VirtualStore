bd2f56e830af7029af29c13308805a302e051823
Artur Cirino <artur.cirino@gmail.com>
2023-05-30 18:03:42 -0300

    Added FeaturedProducts in Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,17 @@
 import Banner from "../components/Banner/Banner";
 import Information from "../components/Information/Information";
+import FeaturedProducts from "../components/FeaturedProducts/FeaturedProducts";
+import { Container } from "react-bootstrap";
+import products from "../data";
 
 function Home() {
   return (
     <div>
       <Banner />
       <Information />
+      <Container>
+        <FeaturedProducts products={products} />
+      </Container>
     </div>
   );
 }
