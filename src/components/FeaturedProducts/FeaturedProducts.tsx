a097dc853d079ad2a26c6237edc3e5a244c4811a
Artur Cirino <artur.cirino@gmail.com>
2023-06-01 15:46:57 -0300

    Getting data from the api in Home

--- src/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -7,23 +7,27 @@ interface FeaturedProductsProps {
 }
 
 function FeaturedProducts({ products }: FeaturedProductsProps) {
-  const featuredProducts = products.filter((product) => product.destaque).slice(0, 4);
+  if (!Array.isArray(products)) {
+    return null; // ou tratamento de erro adequado
+  }
+
+  const featuredProducts = products.filter((product) => product.featured).slice(0, 4);
 
   return (
     <Container>
       <h3 className="text-center mt-4 mb-4">Produtos em Destaque</h3>
       <Row>
         {featuredProducts.map((product) => (
-          <Col key={product.id} md={3}>
+          <Col key={product.slug} md={3}>
             <Card className="mb-4" style={{ boxShadow: "3px 3px 10px #ccc" }}>
               <Card.Text
                 className="fs-5 m-0 p-2 text-dark fw-semibold align-items-center text-center"
                 style={{ height: "75px", backgroundColor: "#ededed" }}
               >
-                {product.title}
+                {product.name}
               </Card.Text>
               <Link to={`/produtos/${product.slug}`}>
-                <Card.Img variant="top" src={product.images[0]} alt={product.title} />
+                <Card.Img variant="top" src={product.images[0]} alt={product.name} />
               </Link>
               <Card.Text className="fs-5 p-1 fw-semibold p-2" style={{ color: "#873143", backgroundColor: "#ededed" }}>
                 Preço: R$ {product.price.toFixed(2)}
