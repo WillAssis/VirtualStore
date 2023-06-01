0f754f030c3eea49c78716e273f86939dccc8a0a
Artur Cirino <artur.cirino@gmail.com>
2023-06-01 15:50:45 -0300

    Getting data from the api in Products and refactoring component

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,26 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Col, Container, Row } from "react-bootstrap";
-import products from "../data";
 import ProductCard from "../components/ProductCard/ProductCard";
-import ProductSearch from "../components/ProductSearch/ProductSearch";
 import Pagination from "../components/Pagination/Pagination";
+import { Product } from "../types";
 
 function Products() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
+  const [pages, setPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
-  const productsPerPage = 8;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [error, setError] = useState("");
 
-  // Filtra os produtos com base na pesquisa
-  const [filteredProducts, setFilteredProducts] = useState(products);
+  useEffect(() => {
+    fetch(`http://localhost:3333/produtos?page=${currentPage}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+        setPages(data.pages);
+      })
+      .catch((error) => {
+        console.log("Ocorreu um erro ao obter os produtos:", error);
+      });
+  }, [currentPage]);
 
-  // Lógica de paginação
-  const indexOfLastProduct = currentPage * productsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
-  const currentProducts = filteredProducts.slice(indexOfFirstProduct, indexOfLastProduct);
+  useEffect(() => {
+    const filteredProducts = products.filter((product) => (product.name ?? "").toLowerCase().includes(searchTerm.toLowerCase()));
 
-  // Função para mudar a página atual
-  const paginate = (pageNumber: number) => {
-    setCurrentPage(pageNumber);
+    setFilteredProducts(filteredProducts);
+  }, [products, searchTerm]);
+
+  const handlePageChange = (page: number) => {
+    setCurrentPage(page);
   };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const goToNextPage = () => {
+    if (currentPage < pages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    if (filteredProducts.length === 0 && searchTerm !== "") {
+      setError(`Nenhum produto encontrado com o nome ${searchTerm}`);
+    } else {
+      setError("");
+    }
+  }, [filteredProducts, searchTerm]);
 
   return (
     <>
@@ -28,19 +64,38 @@ function Products() {
         Mostrando Produtos
       </h3>
       <Container>
-        <ProductSearch products={products} setFilteredProducts={setFilteredProducts} />
         <Row>
-          {currentProducts.map((product) => (
-            <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
-              <ProductCard product={product} />
+          <Col xs={12}>
+            <div className="shadow-sm">
+              <input
+                type="text"
+                className="w-100 p-1 border-dark border-opacity-10"
+                placeholder="Pesquisar produtos"
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          {error ? (
+            <Col xs={12} className="text-center">
+              <p>{error}</p>
             </Col>
-          ))}
+          ) : (
+            (searchTerm ? filteredProducts : products).map((product) => (
+              <Col xs={12} sm={6} md={4} lg={3}>
+                <ProductCard product={product} />
+              </Col>
+            ))
+          )}
         </Row>
         <Pagination
           currentPage={currentPage}
-          productsPerPage={productsPerPage}
-          totalProducts={filteredProducts.length}
-          paginate={paginate}
+          pages={pages}
+          onPageChange={handlePageChange}
+          onPreviousPage={goToPreviousPage}
+          onNextPage={goToNextPage}
         />
       </Container>
     </>
