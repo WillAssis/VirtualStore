597705570d5f3c68a88f477b77094fcde86a6348
wsfer <93054415+wsfer@users.noreply.github.com>
2023-09-16 11:18:01 -0300

    refactor: minor changes on product page title

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 import Pagination from '../../components/Pagination/Pagination';
 import SearchBar from './subcomponents/SearchBar';
 import ProductList from './subcomponents/ProductList';
@@ -68,23 +67,25 @@ function Products() {
 
   return (
     <main className="product-page">
-      <h3
-        style={{ backgroundColor: '#D9D9D9' }}
-        className="mt-3 mb-3 text-center p-2"
-      >
+      <h2>
+        <svg
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12,18H6V14H12M21,14V12L20,7H4L3,12V14H4V20H14V14H18V20H20V14M20,4H4V6H20V4Z" />
+        </svg>
         Mostrando Produtos
-      </h3>
-      <Container>
-        <SearchBar search={handleSearch} />
-        <ProductList products={products} />
-        <Pagination
-          currentPage={currentPage}
-          pages={pages}
-          onPageChange={handlePageChange}
-          onPreviousPage={goToPreviousPage}
-          onNextPage={goToNextPage}
-        />
-      </Container>
+      </h2>
+      <SearchBar search={handleSearch} />
+      <ProductList products={products} />
+      <Pagination
+        currentPage={currentPage}
+        pages={pages}
+        onPageChange={handlePageChange}
+        onPreviousPage={goToPreviousPage}
+        onNextPage={goToNextPage}
+      />
     </main>
   );
 }
