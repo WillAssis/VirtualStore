33a5a36bb9d2c35c1b6bf549ac8fbfbd0ee5f977
wsfer <93054415+wsfer@users.noreply.github.com>
2023-09-22 15:19:30 -0300

    refactor: minor changes on quantity input test file

--- src/components/QuantityInput/QuantityInput.test.tsx
+++ src/components/QuantityInput/QuantityInput.test.tsx
@@ -5,75 +5,57 @@ import '@testing-library/jest-dom';
 
 let params: {
   quantity: number;
-  changeQuantity: () => void;
+  setQuantity: () => void;
 };
 
 beforeEach(() => {
   params = {
     quantity: 3,
-    changeQuantity: jest.fn(),
+    setQuantity: jest.fn(),
   };
 });
 
-describe('Render input value', () => {
-  test('Default value should be 1, if no quantity parameter', () => {
-    render(<QuantityInput changeQuantity={params.changeQuantity} />);
-    const input = screen.getByRole('spinbutton');
-    expect(input).toHaveValue(1);
-  });
-
-  test('Value equals to quantity parameter', () => {
-    render(<QuantityInput {...params} />);
-    const input = screen.getByRole('spinbutton');
-    expect(input).toHaveValue(3);
-  });
-});
-
 describe('Custom buttons', () => {
-  test('Increase button add 1 on input value', () => {
+  test('Increase button calls function with correct value', () => {
     render(<QuantityInput {...params} />);
 
-    const input = screen.getByRole('spinbutton');
     const button = screen.getByRole('button', { name: 'Aumentar' });
 
     userEvent.click(button);
-    expect(input).toHaveValue(4);
-    expect(params.changeQuantity).toHaveBeenCalledWith(4);
+    expect(params.setQuantity).toHaveBeenCalledWith(4);
   });
 
-  test('Decrease button remove 1 from input value', () => {
+  test('Decrease button calls function with correct value', () => {
     render(<QuantityInput {...params} />);
 
-    const input = screen.getByRole('spinbutton');
     const button = screen.getByRole('button', { name: 'Diminuir' });
 
     userEvent.click(button);
-    expect(input).toHaveValue(2);
-    expect(params.changeQuantity).toHaveBeenCalledWith(2);
+
+    expect(params.setQuantity).toHaveBeenCalledWith(2);
   });
 
-  test('Input value should not go below 1', () => {
+  test('Decrease button should not call function when quantity is 1', () => {
     params.quantity = 1;
     render(<QuantityInput {...params} />);
 
-    const input = screen.getByRole('spinbutton');
     const button = screen.getByRole('button', {
       name: 'Diminuir',
     }) as HTMLButtonElement;
 
     expect(button.disabled).toBe(true);
-
     userEvent.click(button);
-    expect(input).toHaveValue(1);
-    expect(params.changeQuantity).not.toHaveBeenCalled();
+    expect(params.setQuantity).not.toHaveBeenCalled();
   });
 });
 
 describe('Input default functionality', () => {
-  test('User typing directly on input', () => {
+  test('Input changing should call function with correct value', () => {
     render(<QuantityInput {...params} />);
+
     const input = screen.getByRole('spinbutton');
+
     userEvent.type(input, '1');
-    expect(params.changeQuantity).toHaveBeenCalledWith(31);
+    expect(params.setQuantity).toHaveBeenCalledWith(31);
   });
 });
