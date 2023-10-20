4b4c3789a8bb62462c43221deceed3318d1c589a
wsfer <93054415+wsfer@users.noreply.github.com>
2023-10-20 17:36:38 -0300

    test: create login endpoint on test setup server

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -1,7 +1,17 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.post('http://localhost:3333/login', (req, res, ctx) => {
-    return res(ctx.json({ response: 'fake' }));
+  rest.post('http://localhost:3333/login', async (req, res, ctx) => {
+    const { username } = await req.json();
+
+    return res(
+      ctx.status(200),
+      ctx.json({
+        user: {
+          username,
+          isAdmin: true,
+        },
+      })
+    );
   }),
 ];
