41b81c444dc42832453cb1c9090e86fbd96b2f5e
wsfer <93054415+wsfer@users.noreply.github.com>
2023-10-20 22:33:09 -0300

    test: create register endpoint on test setup server

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -6,12 +6,29 @@ export const handlers = [
 
     return res(
       ctx.status(200),
-      ctx.json({
-        user: {
-          username,
-          isAdmin: true,
-        },
-      })
+      ctx.json({ user: { username, isAdmin: true } })
+    );
+  }),
+  rest.post('http://localhost:3333/cadastro', async (req, res, ctx) => {
+    const { username } = await req.json();
+
+    if (username === 'UsedName') {
+      return res(
+        ctx.status(418),
+        ctx.json({
+          user: null,
+          errors: {
+            usernameError: 'Custom error message from backend',
+            emailError: '',
+            passwordError: '',
+          },
+        })
+      );
+    }
+
+    return res(
+      ctx.status(201),
+      ctx.json({ user: { username, isAdmin: false } })
     );
   }),
 ];
