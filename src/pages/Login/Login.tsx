3ca809c30dd00da0c9627584ace5f14ff8daa735
wsfer <93054415+wsfer@users.noreply.github.com>
2023-10-21 23:15:46 -0300

    feat: add loading screen and redirect to homepage when login or register is successful

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import UsernameInput from '../../components/Inputs/UsernameInput';
 import PasswordInput from '../../components/Inputs/PasswordInput';
+import Loading from '../../components/Loading/Loading';
 import login from '../../utils/login';
 import { User } from '../../types';
 import './Login.css';
@@ -11,26 +12,38 @@ interface Params {
 }
 
 function Login({ setUser }: Params) {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [usernameError, setUsernameError] = useState('');
   const [passwordError, setPasswordError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState('');
 
   async function authUser(event: React.FormEvent<HTMLFormElement>) {
+    setLoading(true);
+    setLoadingMessage('Aguarde');
     event.preventDefault();
     const loginAttempt = await login(username, password);
     const { success, user, errors } = { ...loginAttempt };
 
     if (success) {
       setUser(user);
+      setLoadingMessage(
+        'Logado com sucesso. Redirecionando para a página inicial'
+      );
+      await new Promise((resolve) => setTimeout(resolve, 2500)); // delay
+      navigate('/');
+    } else {
+      setUsernameError(errors.usernameError);
+      setPasswordError(errors.passwordError);
+      setLoading(false);
     }
-
-    setUsernameError(errors.usernameError);
-    setPasswordError(errors.passwordError);
   }
 
   return (
     <main className="login-page">
+      {loading ? <Loading error={loadingMessage} /> : null}
       <section aria-labelledby="login-page-title">
         <form className="login-form" noValidate onSubmit={authUser}>
           <h2 id="login-page-title">Fazer Login</h2>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import UsernameInput from '../../components/Inputs/UsernameInput';
 import PasswordInput from '../../components/Inputs/PasswordInput';
 import EmailInput from '../../components/Inputs/EmailInput';
+import Loading from '../../components/Loading/Loading';
 import register from '../../utils/register';
 import { User } from '../../types';
 import './Register.css';
@@ -11,30 +13,42 @@ interface Params {
 }
 
 function Register({ setUser }: Params) {
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
   const [usernameError, setUsernameError] = useState('');
   const [passwordError, setPasswordError] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [loadingMessage, setLoadingMessage] = useState('');
 
   async function registerUser(event: React.FormEvent<HTMLFormElement>) {
+    setLoading(true);
+    setLoadingMessage('Aguarde');
     event.preventDefault();
     const registerAttempt = await register(username, email, password);
     const { success, user, errors } = { ...registerAttempt };
 
     if (success) {
       setUser(user);
+      setLoadingMessage(
+        'Usuário cadastrado com sucesso. Redirecionando para a página inicial'
+      );
+      await new Promise((resolve) => setTimeout(resolve, 2500)); // delay
+      navigate('/');
+    } else {
+      setUsernameError(errors.usernameError);
+      setPasswordError(errors.passwordError);
+      setEmailError(errors.emailError);
+      setLoading(false);
     }
-
-    setUsernameError(errors.usernameError);
-    setPasswordError(errors.passwordError);
-    setEmailError(errors.emailError);
   }
 
   return (
     <main className="register-page">
-      <form className="register-form" onSubmit={registerUser}>
+      {loading ? <Loading error={loadingMessage} /> : null}
+      <form className="register-form" noValidate onSubmit={registerUser}>
         <h2>Criar conta</h2>
         <UsernameInput
           value={username}
