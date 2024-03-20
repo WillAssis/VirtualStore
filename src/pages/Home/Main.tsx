915de61a7283af70690d33d2180f35b5da327d3b
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-20 16:49:51 -0300

    refactor: Main component on Home page

--- src/pages/Home/Main.tsx
+++ src/pages/Home/Main.tsx
@@ -1,41 +1,52 @@
 import Logo from '../../components/Logo/Logo';
 import ImageButton from '../../components/Buttons/ImageButton';
+import Container from '../../components/Container/Container';
 import productImage from '../../assets/images/home-products-button-background.jpg';
 import loginImage from '../../assets/images/home-login-button-background.jpg';
 import registerImage from '../../assets/images/home-register-button-background.jpg';
-import './Main.css';
+import styles from './Main.module.scss';
 
 function Main() {
   return (
-    <main className="homepage-main">
-      <h1 aria-label="Solus Personalizados">
-        <Logo />
-      </h1>
-      <p>
-        O seu <span className="accent-text">Sonho</span> também é nosso
-      </p>
-      <nav aria-label="Selecione o seu destino">
-        <ul>
-          <li>
-            <ImageButton
-              path="/produtos"
-              text="Ver produtos"
-              image={productImage}
-            />
-          </li>
-          <li>
-            <ImageButton path="/login" text="Fazer login" image={loginImage} />
-          </li>
-          <li>
-            <ImageButton
-              path="/cadastro"
-              text="Cadastrar-se"
-              image={registerImage}
-            />
-          </li>
-        </ul>
-      </nav>
-      <p>Papelaria e artigos personalizados para bodas & casamentos</p>
+    <main className={styles.main}>
+      <Container>
+        <div className={styles.contentWrapper}>
+          <h1 className={styles.title} aria-label="Solus Personalizados">
+            <Logo />
+          </h1>
+          <p className={styles.textTop}>
+            O seu <span className={styles.accent}>Sonho</span> também é nosso
+          </p>
+          <nav className={styles.nav} aria-label="Selecione o seu destino">
+            <ul className={styles.navList}>
+              <li>
+                <ImageButton
+                  path="/produtos"
+                  text="Ver produtos"
+                  image={productImage}
+                />
+              </li>
+              <li>
+                <ImageButton
+                  path="/login"
+                  text="Fazer login"
+                  image={loginImage}
+                />
+              </li>
+              <li>
+                <ImageButton
+                  path="/cadastro"
+                  text="Cadastrar-se"
+                  image={registerImage}
+                />
+              </li>
+            </ul>
+          </nav>
+          <p className={styles.textBottom}>
+            Papelaria e artigos personalizados para bodas & casamentos
+          </p>
+        </div>
+      </Container>
     </main>
   );
 }
