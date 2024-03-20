d30401066976ab10015956fde709aea594d0d967
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-19 23:11:08 -0300

    create useFetch custom hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+function useFetch<Data>(url: string) {
+  const [data, setData] = useState<Data | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>('');
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    fetch(url, { signal: abortController.signal })
+      .then((response) => response.json())
+      .then((data) => setData(data))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+
+    // Cancela a requisição caso uma próxima seja feita para prevenir race condition
+    return () => abortController.abort();
+  }, [url]);
+
+  return { data, loading, error };
+}
+
+export default useFetch;
