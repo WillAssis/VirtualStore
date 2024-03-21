43e1c9b3323b525133870540b98bf2f8d9b54740
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-20 22:53:15 -0300

    refactor: FeaturedProducts component

--- src/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -1,42 +1,27 @@
-import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { Product } from '../../types';
 import { HTTPProductsResponse } from '../../types';
 import useFetch from '../../hooks/useFetch';
 import Loading from '../Loading/Loading';
 import ProductCard from '../Cards/ProductCard';
-import './FeaturedProducts.css';
-
-interface Params {
-  title: string;
-  link?: string;
-}
+import styles from './FeaturedProducts.module.scss';
 
 const DATA_URL = 'http://localhost:3333/destaques';
 
-function FeaturedProducts({ title, link }: Params) {
+function FeaturedProducts() {
   const { data, loading, error } = useFetch<HTTPProductsResponse>(DATA_URL);
 
   const products: Product[] = data?.products.slice(0, 4) || [];
 
   return (
-    <section aria-labelledby="featured-products" className="featured-products">
-      {loading ? (
-        <Loading error={error} />
-      ) : (
-        <>
-          <h2 id="featured-products">{title}</h2>
-          <ul className="products-container">
-            {products.map((product) => (
-              <li key={product.id}>
-                <ProductCard product={product} />
-              </li>
-            ))}
-          </ul>
-          {link ? <Link to={link}>Ver mais</Link> : null}
-        </>
-      )}
-    </section>
+    <Loading loading={loading} error={error}>
+      <ul className={styles.list}>
+        {products.map((product) => (
+          <li key={product.id}>
+            <ProductCard product={product} />
+          </li>
+        ))}
+      </ul>
+    </Loading>
   );
 }
 
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,13 @@
 import Main from './Main';
 import Hero from './Hero';
-import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
+import Featured from './Featured';
 
 function Home() {
   return (
     <>
       <Main />
       <Hero />
-      <FeaturedProducts title="Produtos em destaque" link="/produtos" />
+      <Featured />
     </>
   );
 }
