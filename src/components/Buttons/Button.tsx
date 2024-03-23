0efc340e47b48fdd5044302666b3f0f8f2098bee
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-23 11:29:28 -0300

    feat: add support for onclick event on Button component

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -5,16 +5,17 @@ interface Props {
   text: string;
   path?: string; // Se for passado o botão será um link
   style?: React.CSSProperties; // Quando for necessário modificar algum estilo
+  onClick?: () => void;
 }
 
 // Botão normal com texto
-function Button({ text, path, style }: Props) {
+function Button({ text, path, style, onClick }: Props) {
   return path ? (
     <Link className={styles.button} to={path} style={style}>
       {text}
     </Link>
   ) : (
-    <button className={styles.button} style={style}>
+    <button className={styles.button} onClick={onClick} style={style}>
       {text}
     </button>
   );
