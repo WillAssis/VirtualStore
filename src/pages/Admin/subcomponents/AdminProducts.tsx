505661b05fc2c199be282e80d7b2ae800076eeaf
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-23 22:33:38 -0300

    refactor: AdminProduct page to include new loading and useFetch hook

--- src/pages/Admin/subcomponents/AdminProducts.tsx
+++ src/pages/Admin/subcomponents/AdminProducts.tsx
@@ -1,81 +1,61 @@
 import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { Product, HTTPProductsResponse } from '../../../types';
+import useFetch from '../../../hooks/useFetch';
 import Loading from '../../../components/Loading/Loading';
 import SearchBar from '../../../components/SearchBar/SearchBar';
 import AdminProductCard from '../../../components/Cards/AdminProductCard';
-import { Product } from '../../../types';
 import './AdminProducts.css';
 
+const DATA_URL = 'http://localhost:3333/produtos';
+
 function AdminProducts() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [searchTerm, setSearchTerm] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [rerunFetchChecker, setRerunFetchChecker] = useState(false);
-  const [error, setError] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchURL, setSearchURL] = useState<string>(DATA_URL);
+  const { data, loading, error } = useFetch<HTTPProductsResponse>(searchURL);
+
+  const products: Product[] = data?.products || [];
+  const searchTerm = searchParams.get('search');
 
   useEffect(() => {
-    const searchParam = searchTerm
-      ? '?search='.concat(searchTerm.toLowerCase())
-      : '';
-    const url = `http://localhost:3333/produtos${searchParam}`;
-
-    fetch(url, { credentials: 'include' })
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data.products);
-        // Tempo mínimo de 0.5s para carregar a página
-        setTimeout(() => {
-          setIsLoading(false);
-        }, 500);
-      })
-      .catch((error) => {
-        console.log('Ocorreu um erro ao obter os produtos:', error);
-
-        setTimeout(() => {
-          setRerunFetchChecker(!rerunFetchChecker);
-          setError(
-            'Não foi possível carregar os produtos, verifique sua conexão',
-          );
-        }, 10000);
-      });
-  }, [searchTerm, rerunFetchChecker]);
+    const searchString = searchParams.toString();
+    const newURL = searchString ? `${DATA_URL}?${searchString}` : DATA_URL;
+    setSearchURL(newURL);
+  }, [searchParams]);
 
   const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
-    setIsLoading(true);
     event.preventDefault();
-    const input = document.getElementById('search') as HTMLInputElement;
-    setSearchTerm(input.value);
-    input.value = '';
+    const formData = new FormData(event.currentTarget);
+    const searchString = formData.get('search')?.toString();
+    if (searchString) {
+      searchParams.set('search', searchString);
+    } else {
+      searchParams.delete('search');
+    }
+    searchParams.delete('page');
+    setSearchParams(searchParams);
+    event.currentTarget.value = '';
   };
 
   const deleteProduct = async (slug: string) => {
-    setIsLoading(true);
-
-    const url = `http://localhost:3333/produto/${slug}`;
-    const response = await fetch(url, {
+    const url = `${DATA_URL}/${slug}`;
+    await fetch(url, {
       credentials: 'include',
       method: 'DELETE',
     });
-
-    if (response.ok) {
-      setRerunFetchChecker(!rerunFetchChecker); // Recarrega a lista
-    } else {
-      setError(`Não foi possível deletar o produto: ${response.statusText}`);
-    }
   };
 
   return (
     <main className="admin-products">
-      {isLoading ? (
-        <Loading error={error} />
-      ) : (
+      <Loading loading={loading} error={error}>
         <div className="product-list-wrapper">
           <h2>Listando Produtos</h2>
           <div className="product-list-controls">
             <SearchBar search={handleSearch} />
             <Link to="/admin/produtos/criar">Novo</Link>
           </div>
-          {searchTerm ? <p>Resultados para "{searchTerm}"</p> : null}
+          {searchTerm && <p>Resultados para "{searchTerm}"</p>}
           {products.length > 0 ? (
             <ul className="product-list" aria-label="Lista de produtos">
               {products.map((product) => (
@@ -91,7 +71,7 @@ function AdminProducts() {
             <p>Sem resultados</p>
           )}
         </div>
-      )}
+      </Loading>
     </main>
   );
 }
