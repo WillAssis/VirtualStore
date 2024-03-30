dd8dd9d03694bf1101cdaa99663a20eb5e6ebe4d
wsfer <93054415+wsfer@users.noreply.github.com>
2024-03-30 16:13:26 -0300

    refactor: Cart page

--- src/pages/Cart/ProductList.tsx
+++ src/pages/Cart/ProductList.tsx
@@ -1,28 +1,26 @@
-import CartProductCard from '../../components/Cards/CartProductCard';
 import { CartItem } from '../../types';
-import './ProductList.css';
+import CartProductCard from '../../components/Cards/CartProductCard';
+import styles from './ProductList.module.scss';
 
 interface Params {
   products: CartItem[];
-  updateQuantity: (itemId: number, newQuantity: number) => void;
-  deleteProduct: (itemId: number) => void;
+  updateQuantity: (id: number, newQuantity: number) => void;
+  deleteProduct: (id: number) => void;
 }
 
 function ProductList({ products, updateQuantity, deleteProduct }: Params) {
   return (
-    <section className="cart-items" aria-label="Produtos">
-      <ul className="cart-items-list">
-        {products.map((product) => (
-          <li key={product.id}>
-            <CartProductCard
-              product={product}
-              outerUpdateQuantity={updateQuantity}
-              deleteProduct={deleteProduct}
-            />
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul className={styles.list} aria-label="Produtos">
+      {products.map((product) => (
+        <li key={product.id}>
+          <CartProductCard
+            product={product}
+            outerUpdateQuantity={updateQuantity}
+            deleteProduct={deleteProduct}
+          />
+        </li>
+      ))}
+    </ul>
   );
 }
 
