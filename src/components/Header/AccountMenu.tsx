2018a8046341f56fc18b3f30fa560e676d7e5958
wsfer <93054415+wsfer@users.noreply.github.com>
2024-04-01 23:56:43 -0300

    feat: create user menu component for header

---
+++ src/components/Header/AccountMenu.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useContext, useRef } from 'react';
+import { authContext } from '../../contexts/authContext';
+import { Link } from 'react-router-dom';
+import styles from './AccountMenu.module.scss';
+
+function AccountMenu() {
+  const { user, logout } = useContext(authContext);
+  const menu = useRef<HTMLDetailsElement | null>(null);
+
+  if (!user) return null;
+
+  // O component não faz uso de state. O status open/closed é manipulado diretamente no elemento
+  const closeMenuEvent = (event: MouseEvent) => {
+    const target = event.target as HTMLElement;
+    const menuElement = menu.current;
+
+    if (menuElement && !menuElement.contains(target)) {
+      menuElement.open = false;
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('click', closeMenuEvent);
+    return () => document.body.removeEventListener('click', closeMenuEvent);
+  }, []);
+
+  return (
+    <details ref={menu} className={styles.details}>
+      <summary className={styles.summary} aria-label="Menu do usuário">
+        <svg
+          className={styles.icon}
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+        >
+          <path d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,14C16.42,14 20,15.79 20,18V20H4V18C4,15.79 7.58,14 12,14Z" />
+        </svg>
+      </summary>
+      <nav className={styles.menu}>
+        <p className={styles.menuTitle}>Olá, {user.username}</p>
+        {user.isAdmin && (
+          <Link className={styles.button} to="/admin">
+            Admin
+          </Link>
+        )}
+        <Link className={styles.button} to="/pedidos">
+          Pedidos
+        </Link>
+        <button className={styles.button} onClick={logout}>
+          Sair
+        </button>
+      </nav>
+    </details>
+  );
+}
+
+export default AccountMenu;
