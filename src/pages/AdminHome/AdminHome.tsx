ba3a96618229b59dc45a1903b5cca6c954ec3e80
wsfer <93054415+wsfer@users.noreply.github.com>
2024-04-06 17:00:45 -0300

    feat: create a temporary AdminHome component

---
+++ src/pages/AdminHome/AdminHome.tsx
@@ -0,0 +1,11 @@
+import styles from './AdminHome.module.scss';
+
+function AdminHome() {
+  return (
+    <>
+      <h1 className={styles.title}>Em construção...</h1>
+    </>
+  );
+}
+
+export default AdminHome;
