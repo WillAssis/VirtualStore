710f1d392cddab299384164bc1dd5aed0c7dbda1
wsfer <93054415+wsfer@users.noreply.github.com>
2024-04-30 23:37:02 -0300

    feat: create SelectInput component

---
+++ src/components/Inputs/SelectInput.tsx
@@ -0,0 +1,30 @@
+import styles from './SelectInput.module.scss';
+
+interface Props {
+  name: string;
+  label: string;
+  values: any[];
+  labels: string[];
+  setValue: React.Dispatch<React.SetStateAction<any>>;
+}
+
+function SelectInput({ name, label, values, labels, setValue }: Props) {
+  const onInput = (event: React.FormEvent<HTMLSelectElement>) => {
+    setValue(event.currentTarget.value);
+  };
+
+  return (
+    <div className={styles.inputWrapper}>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <select className={styles.select} name={name} id={name} onInput={onInput}>
+        {values.map((value, index) => (
+          <option value={value}>{labels[index]}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default SelectInput;
