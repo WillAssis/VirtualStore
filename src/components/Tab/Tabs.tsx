2d3475c56510459ef11f462c7949769cc4ff9426
wsfer <93054415+wsfer@users.noreply.github.com>
2024-04-30 23:35:13 -0300

    feat: create Tabs component

---
+++ src/components/Tab/Tabs.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import styles from './Tabs.module.scss';
+
+interface Props {
+  labels: string[];
+  elements: string[] | JSX.Element[];
+}
+
+function Tabs({ labels, elements }: Props) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  return (
+    <div>
+      <nav>
+        <ul className={styles.list}>
+          {labels.map((label, index) => (
+            <li>
+              <button
+                onClick={() => setActiveIndex(index)}
+                className={
+                  activeIndex === index
+                    ? `${styles.button} ${styles.buttonActive}`
+                    : styles.button
+                }
+              >
+                {label}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div>
+        {elements.map((element, index) => (
+          <div
+            className={
+              activeIndex === index
+                ? `${styles.tab} ${styles.tabActive}`
+                : styles.tab
+            }
+          >
+            {element}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tabs;
